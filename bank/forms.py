b2a4881305f8d1c9aa9450438c5abf2b82587f16
Dave Brougham <d.brougham1@gmail.com>
2024-09-16 19:45:27 +0100

    Fixed data entry

--- bank/forms.py
+++ bank/forms.py
@@ -8,3 +8,16 @@ class IdeaForm(forms.ModelForm):
     class Meta:
         model = Idea
         fields = ["name", "description", "complexity", "effort", "upside", "downside", "notes"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['complexity'].required = False
+        self.fields['effort'].required = False
+        self.fields['upside'].required = False
+        self.fields['downside'].required = False
+        self.fields['notes'].required = False
+
+class IdeaCreateForm(forms.ModelForm):
+    class Meta:
+        model = Idea
+        fields = ["name", "description"]
--- bank/views.py
+++ bank/views.py
@@ -1,10 +1,10 @@
 from django.views.decorators.http import require_POST
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
-from django.forms import inlineformset_factory
 from .models import Idea, Link
-from .forms import IdeaForm
+from .forms import IdeaForm, LinkInlineFormSet, IdeaCreateForm
 import json
+import logging
 
 def dashboard(request):
     ideas = Idea.objects.all().order_by('order')
@@ -15,12 +15,14 @@ def dashboard(request):
 
 def create(request):
     if request.method == 'POST':
-        form = IdeaForm(request.POST)
+        form = IdeaCreateForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect('dashboard')
+            idea = form.save()
+            return redirect('idea_detail', idea_id=idea.id)
+        else:
+            print(form.errors)
     else:
-        form = IdeaForm()
+        form = IdeaCreateForm()
     return render(request, 'create.html', {'form': form})
 
 def update_idea(request, idea_id):
@@ -46,34 +48,16 @@ def delete_idea(request, idea_id):
 
 def idea_detail(request, idea_id):
     idea = get_object_or_404(Idea, id=idea_id)
-    LinkFormSet = inlineformset_factory(Idea, Link, fields=['url'], extra=1, can_delete=True)
-    
     if request.method == 'POST':
         form = IdeaForm(request.POST, instance=idea)
-        link_formset = LinkFormSet(request.POST, instance=idea)
-        
+        link_formset = LinkInlineFormSet(request.POST, instance=idea)
         if form.is_valid() and link_formset.is_valid():
             form.save()
-            
-            # Check if all existing links are marked for deletion
-            existing_links = Link.objects.filter(idea=idea)
-            all_deleted = all(
-                form.cleaned_data.get('DELETE', False) 
-                for form in link_formset.forms 
-                if form.instance.pk
-            )
-            
-            if all_deleted and existing_links.exists():
-                # If all links are deleted, remove them manually
-                existing_links.delete()
-            else:
-                # Otherwise, save the formset as usual
-                link_formset.save()
-            
+            link_formset.save()
             return redirect('idea_detail', idea_id=idea.id)
     else:
         form = IdeaForm(instance=idea)
-        link_formset = LinkFormSet(instance=idea)
+        link_formset = LinkInlineFormSet(instance=idea)
     
     return render(request, 'idea_detail.html', {
         'idea': idea,
